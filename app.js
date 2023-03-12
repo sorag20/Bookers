8ad5a907fc2f868e2c6a08edbcef097a65d95c08
sorag <gkthn20@gmail.com>
2023-03-12 18:22:25 +0900

    update js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
-
 const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -10,23 +11,103 @@ const connection = mysql.createConnection({
   database: 'Pile_up'
 });
 
-connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('success');
-  });
-
-  
   app.get('/', (req, res) => {
-    connection.query(
-      'SELECT * FROM users',
-      (error, results) => {
-        console.log(results);
-        res.render('hello.ejs');
+        res.render('top.ejs');
+      });
+
+  app.get('/list', (req, res) => {
+        connection.query(
+          'SELECT * FROM users',
+          (error, results) => {
+            res.render('list.ejs', { users: results });
+          }
+        );
+      });
+
+app.get('/register',(req,res)=>{
+        res.render('register.ejs',{errors:[]});
       }
     );
-  });
-  
+
+app.post('/register', 
+  (req, res, next) => {
+    const Name = req.body.name;
+    const total= req.body.total_pages;
+    const errors = [];
+    if (Name === '') {
+      errors.push('本の題名が空です');
+    }
+    if (total=== '') {
+      errors.push('総ページ数が空です');
+    }
+    if (errors.length > 0) {
+      res.render('register.ejs', { errors: errors });
+    } else {
+      next();
+    }
+  },
+  (req, res, next) => {
+    const Name = req.body.name;
+    const errors = [];
+    connection.query(
+      'SELECT * FROM users WHERE name = ?',
+      [Name],
+      (error, results) => {
+        if (results.length > 0) {
+          errors.push('すでに登録されています');
+          res.render('register.ejs', { errors: errors });
+        } else {
+          next();
+        }
+      }
+    );
+  },
+  (req, res) => {
+    const Name = req.body.name;
+    const total = req.body.total_pages;
+    const finished = req.body.finished_pages;
+    connection.query(
+      'INSERT INTO users (name, total_pages, finished_pages) VALUES (?, ?, ?)',
+      [Name, total, finished],
+      (error, results) => {
+          res.redirect('/list');
+        }
+      );
+    });
+
+    app.post('/delete/:id', (req, res) => {
+      connection.query(
+        'DELETE FROM users WHERE id = ?',
+        [req.params.id],
+        (error, results) => {
+          res.redirect('/list');
+        }
+      );
+    });
+   
+    app.get('/edit/:id', (req, res) => {
+      connection.query(
+        'SELECT * FROM users WHERE id = ?',
+        [req.params.id],
+        (error, results) => {
+          res.render('edit.ejs', {book: results[0]});
+        }
+      );
+    });
+
+    app.post('/update/:id', (req, res) => {
+      connection.query('update users set name=?,total_pages=?,finished_pages=?where id=?',
+           [req.body.name,req.body.total_pages,req.body.finished_pages,req.params.id],
+           (error,results)=>{
+             res.redirect('/list');
+           }
+        );
+    });
+    
+    
+
+
+
+
+
   app.listen(3000);
